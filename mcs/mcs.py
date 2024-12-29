2ab58bab40fdcaa79d511cf137ce892e0bf4ab2a
Marek <mail@marek.onl>
2024-12-29 12:38:06 +0100

    Switch to `gpiod`

--- mcs/mcs.py
+++ mcs/mcs.py
@@ -1,76 +1,155 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
+import os
 import sys
+import logging as log
+
 from time import sleep
 from datetime import datetime
 from enum import Enum, auto
+from typing import Any, Optional, Self
 
 import requests
 from requests.auth import HTTPDigestAuth
 
-import SUNXI_GPIO as GPIO
-
-BLUE_LED = "/sys/class/leds/cubieboard2:blue:usr/brightness"
-GREEN_LED = "/sys/class/leds/cubieboard2:green:usr/brightness"
-
-TARIFF_PIN = GPIO.PD7
-FAN_PIN = GPIO.PD3
-
-GPIO.init()
-GPIO.setcfg(TARIFF_PIN, GPIO.IN)
-GPIO.setcfg(FAN_PIN, GPIO.OUT)
+import gpiod
+from gpiod.line import Direction, Value
 
 
-def flatten(array):
+def flatten(array: list[list[Any]]) -> list[Any]:
     return [e for sub_array in array for e in sub_array]
 
 
-def printt(s: str):
-    print(datetime.now().strftime("%Y-%m-%d %H:%M:%S") + f": {s}", flush=True)
+def temps_for_miners(ips: list[str]) -> list[list[int]]:
+    def temps_for_miner(hostname: str) -> list[int]:
+        response = requests.post(
+            "http://" + hostname + "/cgi-bin/miner_stats.cgi",
+            auth=HTTPDigestAuth("root", "pass"),
+        ).json()
+
+        return [response["STATS"][1]["temp2_" + str(i)] for i in range(1, 5)]
+
+    temps = []
+
+    for ip in ips:
+        try:
+            temps.append(temps_for_miner(ip))
+        except Exception:
+            log.warning(f"could not retrieve temperatures from {ip}")
+            continue
+
+    return temps
 
 
-class Tariff:
+class Line:
+    def __init__(self, name) -> None:
+        def gpio_chips():
+            for entry in os.scandir("/dev/"):
+                if gpiod.is_gpiochip_device(entry.path):
+                    yield entry.path
+
+        # Names are not guaranteed unique, so this finds the first line with
+        # the given name.
+        for path in gpio_chips():
+            with gpiod.Chip(path) as chip:
+                try:
+                    self.offset = chip.line_offset_from_id(name)
+                    self.chip_path = "/dev/" + chip.get_info().name
+                    return
+                except OSError:
+                    continue
+
+        raise Exception("gpio line '{}' not found".format(name))
+
+    def value(self, value: Optional[Value] = None) -> Value:
+        direction = Direction.INPUT if value is None else Direction.OUTPUT
+        config = {self.offset: gpiod.LineSettings(direction)}
+
+        try:
+            with gpiod.request_lines(self.chip_path, config) as line:
+                if value is None:
+                    return line.get_value(self.offset)
+                else:
+                    line.set_value(self.offset, value)
+                    return value
+        except Exception as e:
+            raise e
+
+
+class Meter:
     class State(Enum):
         HIGH = auto()
         LOW = auto()
 
-    def __init__(self):
+    def _is_closed(self) -> bool:
+        try:
+            return bool(self.line.value())
+        except Exception:
+            log.warning("could not check if the meter is closed or open")
+            raise
+
+    def _is_open(self) -> bool:
+        return not self._is_closed()
+
+    def _new_state(self) -> State:
+        return self.State.LOW if self._is_open() else self.State.HIGH
+
+    def __init__(self, line: Line) -> None:
+        self.line = line
         self.state = None
 
-    def update(self, tariff: int):
-        new_state = self.State.LOW if tariff == 1 else self.State.HIGH
+        try:
+            self.state = self._new_state()
+        except Exception:
+            log.error("could not initialize the meter status")
+            raise
 
-        if self.state == new_state:
+        log.info("initialized the meter")
+
+        match self.state:
+            case self.State.LOW:
+                log.info("initial price rate is low")
+            case self.State.HIGH:
+                log.info("initial price rate is high")
+
+    def update(self) -> None:
+        new_state = None
+
+        try:
+            new_state = self._new_state()
+        except Exception:
+            log.warning("could not update the meter status")
+
+        if self.state == new_state or new_state is None:
             return
 
         match new_state:
             case self.State.LOW:
-                with open(BLUE_LED, "w") as b, open(GREEN_LED, "w") as g:
-                    b.write("0")
-                    b.flush()
-                    g.write("1")
-                    g.flush()
-                    printt("Tariff changed to low")
+                log.info("price rate changed to low")
             case self.State.HIGH:
-                with open(BLUE_LED, "w") as b, open(GREEN_LED, "w") as g:
-                    b.write("1")
-                    b.flush()
-                    g.write("0")
-                    g.flush()
-                    printt("Tariff changed to high")
-            case _:
-                pass
+                log.info("price rate changed to high")
 
         self.state = new_state
 
 
-class Fan:
+class Relay:
     class State(Enum):
         ON = auto()
         OFF = auto()
 
-    def __init__(self):
-        self.stop()
+    def __init__(self, line: Line, lower_threshold: int, upper_threshold: int):
+        self.line = line
+        self.lower_temp_threshold = lower_threshold
+        self.upper_temp_threshold = upper_threshold
+        self.state = self.State.OFF
+
+        try:
+            self.line.value(Value.INACTIVE)
+        except Exception:
+            log.error("could not initialize the relay controller")
+            raise
+
+        log.info("initialized the relay controller")
 
     def is_on(self) -> bool:
         return self.state == self.State.ON
@@ -79,81 +158,76 @@ class Fan:
         return self.state == self.State.OFF
 
     def start(self):
+        try:
+            self.line.value(Value.ACTIVE)
+        except Exception:
+            log.error("could not open the valve and start the fan")
+            return
+
         self.state = self.State.ON
-        GPIO.output(FAN_PIN, GPIO.HIGH)
-        printt("Starting fan")
+
+        log.info("opening valve and starting fan")
 
     def stop(self):
+        try:
+            self.line.value(Value.INACTIVE)
+        except Exception:
+            log.error("could not close the valve and stop the fan")
+            return
+
         self.state = self.State.OFF
-        GPIO.output(FAN_PIN, GPIO.LOW)
-        printt("Stopping fan")
+
+        log.info("closing valve and stopping fan")
 
     def control(self, temp: int):
-        if temp > 107 and not self.is_on():
+        if temp > self.upper_temp_threshold and self.is_off():
             self.start()
-        elif temp < 103 and not self.is_off():
+        elif temp < self.lower_temp_threshold and self.is_on():
             self.stop()
 
 
-def retrieve_temps_for_miner(hostname: str):
-    try:
-        response = requests.post(
-            "http://" + hostname + "/cgi-bin/miner_stats.cgi",
-            auth=HTTPDigestAuth("root", "pass"),
-        ).json()
-    except Exception as e:
-        raise e
-
-    return [response["STATS"][1]["temp2_" + str(i)] for i in range(1, 5)]
-
-
-def blink():
-    with open(BLUE_LED, "r+") as b, open(GREEN_LED, "r+") as g:
-        b_prev_state = b.read()
-        g_prev_state = g.read()
-        b.write("0")
-        b.flush()
-        g.write("0")
-        g.flush()
-        sleep(1)
-        b.write(b_prev_state)
-        b.flush()
-        g.write(g_prev_state)
-        g.flush()
+# # Main Logic
 
 
 def main():
-    tariff = Tariff()
-    fan = Fan()
+    log.basicConfig(
+        level=log.INFO,
+        format="%(asctime)s %(levelname)s %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
 
-    printt("Mining control system started")
+    log.info("initializing mining control system")
+
+    meter = None
+    relay = None
+
+    try:
+        meter = Meter(Line("GPIO23"))
+        relay = Relay(Line("GPIO24"), 102, 106)
+    except Exception as e:
+        log.error("could not initialize the mining control system")
+        log.exception(e)
+        sys.exit(0)
 
     while True:
         try:
             # Retrieve the temperatures
-            ips = ["192.168.5." + last_octet for last_octet in ["5", "6", "7"]]
-            temps = []
+            ips = ["192.168.1." + last_octet for last_octet in ["5", "6", "7"]]
+            temps = temps_for_miners(ips)
 
-            for ip in ips:
-                try:
-                    temps.append(retrieve_temps_for_miner(ip))
-                except Exception:
-                    printt(f"Could not retrieve the temperatures from {ip}")
-                    continue
+            # Control the relay.
+            relay.control(max(flatten(temps)))
 
-            # Control the fan
-            fan.control(max(flatten(temps)))
-
-            # Handle the tariff
-            tariff.update(GPIO.input(TARIFF_PIN))
-
-            # Blink LEDs
-            blink()
+            # Update the meter status.
+            meter.update()
 
             sleep(1)
 
         except KeyboardInterrupt:
+            log.info("terminating mining control system")
             sys.exit(0)
+        except Exception:
+            log.error("unexpected exception, maintenance required")
 
 
 if __name__ == "__main__":
